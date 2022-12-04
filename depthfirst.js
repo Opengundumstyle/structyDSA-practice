bf09b4e0c0b1d197bc170845e774eafe4591a353
Zhile Lin <zhlin04@gmail.com>
2022-12-04 11:24:37 -0800

    bfs

--- depthfirst.js
+++ depthfirst.js
@@ -12,12 +12,24 @@
 // }
 
 // dfs recursive
-const depthFirstPrint = (graph,source) =>{
-        console.log(source)
-        for(let neighbor of graph[source]){
-               depthFirstPrint(graph,neighbor)
+// const depthFirstPrint = (graph,source) =>{
+//         console.log(source)
+//         for(let neighbor of graph[source]){
+//                depthFirstPrint(graph,neighbor)
+//         }
+//     }
+
+// bfs  iterative
+const breadthFirstPrint = (graph,source) =>{
+       const queue = [source];
+       while(queue.length > 0){
+         const current = queue.shift();
+         console.log(current)
+        for(let neighbor of graph[current]){
+             queue.push(neighbor)
         }
-    }
+       }
+}
 
 const graph ={
     a:['b','c'],
