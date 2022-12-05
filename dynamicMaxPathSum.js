1f35d74af0b894777bae4a9159eb66b64ca58b87
Zhile Lin <zhlin04@gmail.com>
2022-12-05 14:10:18 -0800

    with memo

--- dynamicMaxPathSum.js
+++ dynamicMaxPathSum.js
@@ -71,14 +71,32 @@ maxPathSum(grid); // -> 56
 
 // inefficient way
 
-const maxPathSum = (grid,r = 0,c = 0) => {
+// const maxPathSum = (grid,r = 0,c = 0) => {
+//     // todo
+//        if(r === grid.length  || c === grid[0].length) return -Infinity;
+//        if(r === grid.length - 1 && c === grid[0].length -1) return grid[r][c]
+      
+//        const down =  maxPathSum(grid,r + 1,c)
+//        const right =  maxPathSum(grid,r,c + 1)
+       
+//        return grid[r][c] = Math.max(down,right)
+       
+//   };
+
+// with memo
+
+const maxPathSum = (grid,r = 0,c = 0,memo={}) => {
     // todo
+      let pos =r +','+ c
+      
+       if(pos in memo) return memo[pos]
        if(r === grid.length  || c === grid[0].length) return -Infinity;
        if(r === grid.length - 1 && c === grid[0].length -1) return grid[r][c]
       
-       const down =  maxPathSum(grid,r + 1,c)
-       const right =  maxPathSum(grid,r,c + 1)
+       const down =  maxPathSum(grid,r + 1,c,memo)
+       const right =  maxPathSum(grid,r,c + 1,memo)
        
-       return grid[r][c] = Math.max(down,right)
+       memo[pos] = grid[r][c] + Math.max(down,right)
+       return memo[pos]
        
   };
