54bb66394f13adec455561b148f5e660aa772900
Zhile Lin <zhlin04@gmail.com>
2022-12-05 19:03:36 -0800

    bfs iterative

--- hasPath.js
+++ hasPath.js
@@ -66,20 +66,38 @@ const graph = {
 hasPath(graph, 'v', 'z'); // false
 */
 // dfs iterative
-const hasPath = (graph, src, dst) => {
+// const hasPath = (graph, src, dst) => {
+//     // todo
+//     let stack = [src]
+//     while(stack.length > 0){
+//         let current  = stack.pop()
+        
+//         for(let neighbor of graph[current]){
+//             if(neighbor === dst ){
+//                  return true
+//             }
+//             stack.push(neighbor)
+//         }
+//     }
+//     return false
+//   };
+  
+  // dfs recursive 
+  
+
+  // bfs iterative
+  const hasPath = (graph, src, dst) => {
     // todo
-    let stack = [src]
-    while(stack.length > 0){
-        let current  = stack.pop()
+    let queue = [src]
+    while(queue.length > 0){
+        let current  = queue.shift()
         
         for(let neighbor of graph[current]){
             if(neighbor === dst ){
                  return true
             }
-            stack.push(neighbor)
+            queue.push(neighbor)
         }
     }
     return false
   };
-  
-  
