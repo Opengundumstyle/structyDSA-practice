930259b7c3d53c7e2f089d9a80310d16a7891d29
Zhile Lin <zhlin04@gmail.com>
2022-12-22 12:43:07 -0800

    solution

--- stacks/decompressBraces.js
+++ stacks/decompressBraces.js
@@ -1,11 +1,14 @@
 /*
-Write a function, decompressBraces, that takes in a compressed string as an argument. The function should return the string decompressed.
+Write a function, decompressBraces, that takes in a compressed string as an argument. 
+The function should return the string decompressed.
 
-The compression format of the input string is 'n{subString}', where the subString within braces should be repeated n times.
+The compression format of the input string is 'n{subString}', where the subString within 
+braces should be repeated n times.
 
 You may assume that every number n is guaranteed to be an integer between 1 through 9.
 
-You may assume that the input is valid and the decompressed string will only contain alphabetic characters.
+You may assume that the input is valid and the decompressed string will only contain alphabetic 
+characters.
 
 test_00:
 
@@ -40,3 +43,40 @@ test_07:
 decompressBraces("3{al4{ec}2{icia}}"); 
 // -> alececececiciaiciaalececececiciaiciaalececececiciaicia 
 */
+
+const decompressBraces = (s) => {
+    // todo
+      let stack = []
+      let num = "123456789"
+      for(let char of s){
+          if(num.includes(char)){
+               stack.push(Number(char)) 
+          }else if(char!=='{'){
+               if(char === '}'){
+                    let subStr = ''
+                    while(stack.length > 0){
+                          let newchar = stack.pop()
+                          if(typeof newchar === 'number'){
+                               let newStr = repeatStr(subStr,newchar)
+                                stack.push(newStr)
+                                break
+                          }else{
+                          subStr = newchar + subStr
+                          }
+                    }
+               }else{
+                stack.push(char)
+               } 
+            }
+         }
+          let decompressStr = stack.join('')
+          return decompressStr
+  };
+  
+  const repeatStr =(subStr,num)=>{
+        let newStr = ''
+        for(let i = 0;i < num; i++){
+            newStr += subStr
+        }
+       return newStr
+  }
