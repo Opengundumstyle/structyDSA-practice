09c20fbaeccd5cf80598b821dc971326bc19fe24
Zhile Lin <zhlin04@gmail.com>
2022-12-29 18:33:48 -0800

    neetcode solution

--- leecode/twopinters/containMostWater.js
+++ leecode/twopinters/containMostWater.js
@@ -22,21 +22,52 @@ Input: height = [1,1]
 Output: 1
 */
 
-var maxArea = function(height) {
-    let i = 0;
-    let j = height.length - 1;
-    let maxArea = 0;
-   while(i < j){
-          let currentArea;
-          let width = j - i 
-          if(height[i] >= height[j]){
-                 currentArea = width * height[j]
-                 j--
-          }else {
-                 currentArea = width * height[i]
-                 i ++ 
-         }
-          if(currentArea > maxArea) maxArea = currentArea
-}
-   return maxArea
+// var maxArea = function(height) {
+//     let i = 0;
+//     let j = height.length - 1;
+//     let maxArea = 0;
+//    while(i < j){
+//           let currentArea;
+//           let width = j - i 
+//           if(height[i] >= height[j]){
+//                  currentArea = width * height[j]
+//                  j--
+//           }else {
+//                  currentArea = width * height[i]
+//                  i ++ 
+//          }
+//           if(currentArea > maxArea) maxArea = currentArea
+// }
+//    return maxArea
+// };
+
+// neetcode solution
+
+var maxArea = function (height) {
+    let [left, right, max] = [0, height.length - 1, 0];
+
+    while (left < right) {
+        const [leftHeight, rightHeight] = getHeights(height, left, right);
+        const area = getArea(height, left, right);
+
+        max = Math.max(max, area);
+
+        const isRightGreater = leftHeight <= rightHeight;
+        if (isRightGreater) left++;
+
+        const isRightLess = rightHeight < leftHeight;
+        if (isRightLess) right--;
+    }
+
+    return max;
+};
+
+const getHeights = (height, left, right) => [height[left], height[right]];
+
+const getArea = (height, left, right) => {
+    const [leftHeight, rightHeight] = getHeights(height, left, right);
+    const _height = Math.min(leftHeight, rightHeight);
+    const width = right - left;
+
+    return _height * width;
 };
