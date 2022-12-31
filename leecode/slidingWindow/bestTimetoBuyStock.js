b8f5ffc07d5050932119495ea99f025c12d72c71
Zhile Lin <zhlin04@gmail.com>
2022-12-30 23:42:18 -0800

    solution

--- leecode/slidingWindow/bestTimetoBuyStock.js
+++ leecode/slidingWindow/bestTimetoBuyStock.js
@@ -1,18 +1,20 @@
 /*
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+different day in the future to sell that stock.
 
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any 
+profit, return 0.
 
- 
 
 Example 1:
 
 Input: prices = [7,1,5,3,6,4]
 Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before 
+you sell.
 Example 2:
 
 Input: prices = [7,6,4,3,1]
@@ -20,3 +22,17 @@ Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
 
 */
+
+var maxProfit = function(prices) {//[7,1,5,3,6,4]
+    let buy = 0;
+    let sell = 1;
+    let maxProfit = 0;
+   while(sell < prices.length){
+         let profit = prices[sell] - prices[buy]
+          if(profit < 0) buy = sell
+          if( profit > maxProfit)  maxProfit = profit
+           sell += 1 
+   }
+   return maxProfit
+}
+
