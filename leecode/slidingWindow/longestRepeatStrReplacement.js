e10b0d48c88ab479267c1c0a15870043fd9ec4fc
Zhile Lin <zhlin04@gmail.com>
2023-01-03 13:21:00 -0800

    my solution only beats 10% ...

--- leecode/slidingWindow/longestRepeatStrReplacement.js
+++ leecode/slidingWindow/longestRepeatStrReplacement.js
@@ -25,47 +25,94 @@ The substring "BBBB" has the longest repeating letters, which is 4.
  * @param {number} k
  * @return {number}
  */
-var characterReplacement = function (s, k) {
-    let [left, right, longest, max] = new Array(4).fill(0);
-    const frequencyMap = new Array(26).fill(0);
+// var characterReplacement = function (s, k) {
+//     let [left, right, longest, max] = new Array(4).fill(0);
+//     const frequencyMap = new Array(26).fill(0);
 
-    while (right < s.length) {
-        const count = addRightFrequency(s, right, frequencyMap);
+//     while (right < s.length) {
+//         const count = addRightFrequency(s, right, frequencyMap);
 
-        longest = Math.max(longest, count);
+//         longest = Math.max(longest, count);
 
-        let window = right - left + 1;
-        const canSlide = k < window - longest;
-        if (canSlide) {
-            subtractLeftFrequency(s, left, frequencyMap);
-            left++;
+//         let window = right - left + 1;
+//         const canSlide = k < window - longest;
+//         if (canSlide) {
+//             subtractLeftFrequency(s, left, frequencyMap);
+//             left++;
+//         }
+
+//         window = right - left + 1;
+//         max = Math.max(max, window);
+
+//         right++;
+//     }
+
+//     return max;
+// };
+
+// const addRightFrequency = (s, right, map) => {
+//     const char = s[right];
+//     const index = getCode(char);
+
+//     map[index]++;
+
+//     return map[index];
+// };
+
+// const subtractLeftFrequency = (s, left, map) => {
+//     const char = s[left];
+//     const index = getCode(char);
+
+//     map[index]--;
+
+//     return map[index];
+// };
+
+// const getCode = (char) => char.charCodeAt(0) - 'A'.charCodeAt(0);
+
+
+// my soluiton
+
+var characterReplacement = function(s, k) {
+    let hash = {};
+    let maxlength = 0
+    left = 0 
+    for(let el of s){
+        let sum = 0
+        if(hash[el]){
+             hash[el] += 1
+        }else{
+              hash[el] = 1
         }
-
-        window = right - left + 1;
-        max = Math.max(max, window);
-
-        right++;
+        console.log(hash)
+        let valArr = Object.values(hash)
+        let currentMax = Math.max(...valArr)
+         console.log(valArr)
+         console.log(currentMax)
+        valArr.forEach(item => sum += item)
+        let difference = sum  - currentMax
+        if(difference <= k ) {
+                console.log('does it hit')
+                 if (sum > maxlength) maxlength = sum
+        }else{
+             while(difference > k){
+                 let key =  s[left] 
+                 sum = 0
+                 hash[key] -= 1
+                 left += 1
+                 valArr = Object.values(hash)
+                 console.log(hash)
+                 console.log(valArr)
+                 currentMax = Math.max(...valArr)
+                 valArr.forEach(item => sum += item)
+                 difference = sum  - currentMax
+                 if (sum > maxlength) maxlength = sum
+            }
+         }
     }
-
-    return max;
+    return maxlength
 };
 
-const addRightFrequency = (s, right, map) => {
-    const char = s[right];
-    const index = getCode(char);
+let s = "ABAA"
 
-    map[index]++;
-
-    return map[index];
-};
-
-const subtractLeftFrequency = (s, left, map) => {
-    const char = s[left];
-    const index = getCode(char);
-
-    map[index]--;
-
-    return map[index];
-};
-
-const getCode = (char) => char.charCodeAt(0) - 'A'.charCodeAt(0);
+console.log(characterReplacement(s,0))
