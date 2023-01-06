dd77b5f3e572652649d1bd4fae2e958a51fdcaf6
Zhile Lin <zhlin04@gmail.com>
2023-01-06 15:49:20 -0800

    solution

--- leecode/binarySearch/findminInSortedRotateArr.js
+++ leecode/binarySearch/findminInSortedRotateArr.js
@@ -27,3 +27,14 @@ Input: nums = [11,13,15,17]
 Output: 11
 Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
  */
+
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    while(left < right){
+    let mid = Math.floor((left + right)/ 2)
+    if(nums[right] < nums[mid]) left = mid + 1
+    else right = mid
+    }
+    return nums[left]
+};
