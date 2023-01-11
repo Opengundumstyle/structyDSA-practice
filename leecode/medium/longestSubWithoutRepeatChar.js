7bcd258fb34612c2e26c4cc2232cc0cbcf355bff
Zhile Lin <zhlin04@gmail.com>
2023-01-11 13:34:15 -0800

    solution

--- leecode/medium/longestSubWithoutRepeatChar.js
+++ leecode/medium/longestSubWithoutRepeatChar.js
@@ -22,3 +22,22 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
+
+var lengthOfLongestSubstring = function (s) {
+    let left = 0
+    let longestSub = 0
+    let subSet = new Set()
+    for(let el of s){
+        
+                while(subSet.has(el)){
+                       subSet.delete(s[left])
+                       left += 1
+                }
+          
+               subSet.add(el)
+          
+          if(subSet.size > longestSub) longestSub = subSet.size
+    }
+
+   return longestSub
+};
