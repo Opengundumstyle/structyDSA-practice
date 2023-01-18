e85897fcc78a40f3e1e880b1a06d1fe5f8ba7d8e
Zhile Lin <zhlin04@gmail.com>
2023-01-17 18:38:11 -0800

    solution

--- leecode/Blind75/bitmanipulation/reversebits.py
+++ leecode/Blind75/bitmanipulation/reversebits.py
@@ -17,3 +17,12 @@
 # Output:   3221225471 (10111111111111111111111111111111)
 # Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
 
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for i in range(32):
+              bit = (n >> i) & 1
+              res = res | bit << (31 - i)
+        return res
