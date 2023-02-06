b42982f31a42ddce8b58d814f4ca97955405a22f
Zhile Lin <zhlin04@gmail.com>
2023-02-05 20:17:23 -0800

    solution

--- leecode/easy/palimdromlinklist.py
+++ leecode/easy/palimdromlinklist.py
@@ -21,20 +21,50 @@
 #         self.val = val
 #         self.next = next
 # O(N) space solution
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#              arr = []
+#              current = head
+#              while current:
+#                    arr.append(current.val)
+#                    current = current.next
+            
+#              l,r = 0, len(arr)-1
+            
+#              while l <= r:
+#                   if arr[l] != arr[r]:
+#                         return False
+#                   l += 1
+#                   r -= 1
+
+#              return True
+
+# O(1) space solution
+  # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-             arr = []
-             current = head
-             while current:
-                   arr.append(current.val)
-                   current = current.next
+           fast = head
+           slow = head
+           while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
             
-             l,r = 0, len(arr)-1
-            
-             while l <= r:
-                  if arr[l] != arr[r]:
-                        return False
-                  l += 1
-                  r -= 1
+           prev = None
+           while slow:
+                temp = slow.next
+                slow.next = prev
+                prev = slow
+                slow = temp
 
-             return True
+           left, right = head, prev
+           while right:
+                if left.val != right.val:
+                    return False
+                left = left.next
+                right = right.next
+
+           return True
