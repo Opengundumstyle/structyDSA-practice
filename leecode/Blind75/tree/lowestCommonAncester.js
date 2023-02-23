34b9cd715f114ff44765168f30bc8ca9e37aa3eb
Zhile Lin <zhlin04@gmail.com>
2023-02-23 10:38:41 -0800

    solution

--- leecode/Blind75/tree/lowestCommonAncester.js
+++ leecode/Blind75/tree/lowestCommonAncester.js
@@ -20,3 +20,22 @@
 
 // Input: root = [2,1], p = 2, q = 1
 // Output: 2
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    let current = root
+    while(current){
+          if (p.val < current.val && q.val < current.val){
+                current = current.left
+          }else if(p.val > current.val && q.val > current.val){
+                current = current.right
+          }else{
+                return current
+          }
+    }
+};
