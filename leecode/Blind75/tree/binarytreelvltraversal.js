9298afcb473df1d17ea14d9e634a278222397e2c
Zhile Lin <zhlin04@gmail.com>
2023-02-24 12:03:59 -0800

    fixed solution

--- leecode/Blind75/tree/binarytreelvltraversal.js
+++ leecode/Blind75/tree/binarytreelvltraversal.js
@@ -25,24 +25,31 @@
  * }
  */
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * @param {TreeNode} root
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    let res = []
-    let q = [root]
-    while(q){
-         let level = []
-         let qlen = q.length
-         for(let i = 0; i < qlen;i++){
-            let node = q.shift()
-             if (node){
-                  level.push(node)
-                  q.push(node.left)
-                  q.push(node.right)
-              }
-            }
-          res.push(level)
-    }
-    return res
+    if(!root) return []
+     let res = []
+     let q = [root]
+     while(q.length){
+          let level = []
+          let qlen = q.length
+          for(let i = 0; i < qlen;i++){
+             let node = q.shift()
+                   level.push(node.val)
+                   if (node.left)q.push(node.left)
+                   if (node.right) q.push(node.right)
+             }
+           res.push(level)
+     }
+     return res
 };
