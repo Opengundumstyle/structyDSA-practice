64f356f65736b6eb0c828726eeed167966ee2403
Zhile Lin <zhlin04@gmail.com>
2023-04-29 13:45:42 -0700

    solution

--- leecode/googleInterviews/ArrayAndStr/rotateImage.js
+++ leecode/googleInterviews/ArrayAndStr/rotateImage.js
@@ -22,4 +22,22 @@
 // 1 <= n <= 20
 // -1000 <= matrix[i][j] <= 1000
 
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+   
+    for(let i = 0; i < matrix.length ;i ++){
+       for(let j = i; j < matrix.length ; j ++){
+             [matrix[j][i],matrix[i][j]] = [matrix[i][j],matrix[j][i]]
+       }
+    }
+  
+    for(let row of matrix){
+          row.reverse()
+    }
+    
+};
+
 
