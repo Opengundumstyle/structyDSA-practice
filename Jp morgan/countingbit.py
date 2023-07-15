9ec89d072b4081fd417876ac07687100a62b21aa
Zhile Lin <zhlin04@gmail.com>
2023-07-15 15:10:51 -0700

    my solution

---
+++ Jp morgan/countingbit.py
@@ -0,0 +1,53 @@
+# 338. Counting Bits
+# Easy
+# 9.1K
+# 435
+# company
+# Amazon
+# company
+# Adobe
+# company
+# Goldman Sachs
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# Example 2:
+
+# Input: n = 5
+# Output: [0,1,1,2,1,2]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 3 --> 11
+# 4 --> 100
+# 5 --> 101
+ 
+
+# Constraints:
+
+# 0 <= n <= 105
+ 
+
+# Follow up:
+
+# It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+# Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+
+#my solution
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bitArr = []
+        for num in range(0,n+1):
+            count = bin(num).count('1')
+            bitArr.append(count)
+        return bitArr
