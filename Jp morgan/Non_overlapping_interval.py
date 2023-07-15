10c0211800820a8f787b71ea6af5ef9e67d58cb0
Zhile Lin <zhlin04@gmail.com>
2023-07-15 11:40:13 -0700

    solution

--- Jp morgan/Non_overlapping_interval.py
+++ Jp morgan/Non_overlapping_interval.py
@@ -19,3 +19,17 @@
 # Output: 0
 # Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
 
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        print(intervals)
+        count = 0
+        prevEnd = intervals[0][1]
+        for start,end in intervals[1:]:
+              if start >= prevEnd:
+                   prevEnd = end
+              else:
+                   count += 1
+                   prevEnd = min(end,prevEnd)
+        return count
