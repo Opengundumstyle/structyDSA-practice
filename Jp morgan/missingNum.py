4f40d4017f8d8557d9ee42c70a602f72e3ebd322
Zhile Lin <zhlin04@gmail.com>
2023-07-15 16:30:48 -0700

    solution

--- Jp morgan/missingNum.py
+++ Jp morgan/missingNum.py
@@ -47,3 +47,16 @@
 # 63.2%
 
 
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        missing = len(nums)
+        
+        for i, num in enumerate(nums):
+          
+            missing ^= i ^ num
+        
+        return missing
