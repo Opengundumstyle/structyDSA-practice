bfa200b21e9e0af73abf9d5565a2f8eb621d2cca
Zhile Lin <zhlin04@gmail.com>
2023-07-15 15:01:03 -0700

    neetcode

--- Jp morgan/numberOf1bit.py
+++ Jp morgan/numberOf1bit.py
@@ -33,3 +33,12 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
        count = bin(n).count('1')
        return count
+
+# neetcode solution
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+       res = 0
+       while n :
+          res +=  n % 2
+          n = n >> 1
+       return res  
