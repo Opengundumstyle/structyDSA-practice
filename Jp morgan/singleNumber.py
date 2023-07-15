e0b53717b8e9260ad5899f204629be420db229d3
Zhile Lin <zhlin04@gmail.com>
2023-07-15 14:19:06 -0700

    bit manipulation solution

--- Jp morgan/singleNumber.py
+++ Jp morgan/singleNumber.py
@@ -23,3 +23,11 @@
 # Output: 1
 
 
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+           res = 0
+           for n in nums:
+             res = n ^ res
+           return res
