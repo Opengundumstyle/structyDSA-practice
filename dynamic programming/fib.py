e113e981a212dc651333a8dcf52e8efe7cf5ae5b
Zhile Lin <zhlin04@gmail.com>
2023-07-18 15:58:31 -0700

    soution

--- dynamic programming/fib.py
+++ dynamic programming/fib.py
@@ -34,3 +34,21 @@
 
 # fib(46); // -> 1836311903
 # */
+
+def fib(n):
+   return _fib(n,{})
+
+
+def _fib(n,memo):
+  
+  if n in memo:
+    return memo[n]
+  
+  if n == 0:
+     return 0
+  if n == 1:
+     return 1 
+    
+  memo[n] =  _fib(n-1,memo) + _fib(n-2,memo)
+  return memo[n]
+ 
