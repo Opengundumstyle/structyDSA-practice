a1ac15aff47cb01634eef07b6dec76b0500db12d
Zhile Lin <zhlin04@gmail.com>
2023-07-26 17:30:38 -0700

    op solution

--- dynamic programming/longestpalimdromsub.py
+++ dynamic programming/longestpalimdromsub.py
@@ -35,11 +35,15 @@
 
 
 def max_palin_subsequence(string):
-    return _max_palin_subsequence(string,0,len(string)-1)
+    return _max_palin_subsequence(string,0,len(string)-1,{})
     
 
-def _max_palin_subsequence(string,i,j):
+def _max_palin_subsequence(string,i,j,memo):
   
+    key = (i,j)
+    if key in memo:
+        return memo[key]
+   
     if i == j:
        return 1
     
@@ -47,10 +51,11 @@ def _max_palin_subsequence(string,i,j):
        return 0
     
     if string[i] == string[j]:
-       return 2 + _max_palin_subsequence(string,i+1,j-1)
+      memo[key] =  2 + _max_palin_subsequence(string,i+1,j-1,memo)
     else:
-      return max(
-         _max_palin_subsequence(string,i + 1,j),
-         _max_palin_subsequence(string,i,j - 1)
+      memo[key]= max(
+         _max_palin_subsequence(string,i + 1,j,memo),
+         _max_palin_subsequence(string,i,j - 1,memo)
         )
+    return memo[key] 
       
