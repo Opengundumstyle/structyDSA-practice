bd9bf23d14305facdd3a1425ad065e0b5865984a
Zhile Lin <zhlin04@gmail.com>
2023-07-31 18:58:40 -0700

    stack in python

---
+++ stacks/pairedparentheses.py
@@ -0,0 +1,14 @@
+# paired parentheses
+# Write a function, paired_parentheses, that takes in a string as an argument. The function should return a boolean indicating whether or not the string has well-formed parentheses.
+
+# You may assume the string contains only alphabetic characters, '(', or ')'.
+
+# paired_parentheses("(david)((abby))") # -> True
+# paired_parentheses("()rose(jeff") # -> False
+# paired_parentheses(")(") # -> False
+# paired_parentheses("()") # -> True
+# paired_parentheses("(((potato())))") # -> True
+# paired_parentheses("(())(water)()") # -> True
+# paired_parentheses("(())(water()()") # -> False
+# paired_parentheses("") # -> True
+# paired_parentheses("))()") # False
