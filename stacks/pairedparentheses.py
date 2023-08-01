42f91fd063e2728e945865ce585affbd0bab709f
Zhile Lin <zhlin04@gmail.com>
2023-07-31 19:22:38 -0700

    mysolution

--- stacks/pairedparentheses.py
+++ stacks/pairedparentheses.py
@@ -1,5 +1,6 @@
 # paired parentheses
-# Write a function, paired_parentheses, that takes in a string as an argument. The function should return a boolean indicating whether or not the string has well-formed parentheses.
+# Write a function, paired_parentheses, that takes in a string as an argument. The function should return a boolean 
+#indicating whether or not the string has well-formed parentheses.
 
 # You may assume the string contains only alphabetic characters, '(', or ')'.
 
@@ -12,3 +13,17 @@
 # paired_parentheses("(())(water()()") # -> False
 # paired_parentheses("") # -> True
 # paired_parentheses("))()") # False
+
+def paired_parentheses(string):
+     if string == '': return True
+     if string[0] == ")":
+        return False
+     stack = []
+     for s in string:
+         if s == "(":
+              stack.append(s)
+         if s == ")" and stack[-1] =="(":
+              stack.pop()
+     if len(stack) == 0:
+         return True
+     return False
